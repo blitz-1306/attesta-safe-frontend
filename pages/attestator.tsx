b03b00a02a68a987daa55328faa2882629cb5697
blitz-1306 <hyperx27@gmail.com>
2023-07-20 19:57:52 +0500

    Remove dotEnv due to no real use

--- pages/attestator.tsx
+++ pages/attestator.tsx
@@ -1,9 +1,6 @@
 import {FC} from "react";
 import "react-tabs/style/react-tabs.css";
-import * as dotenv from "dotenv";
 import Attestation from "../components/Attestation/index";
-
-dotenv.config();
 
 const App: FC = () => {
     return (
